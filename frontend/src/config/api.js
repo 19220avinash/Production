cdaaf1103d53495706c76b0cc792e5d2fc636e38
19220avinash <19220avinash@gmail.com>
2026-05-13 10:49:14 +0530

    api

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,3 +1,2 @@
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
-
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000" || "https://production-thm2.onrender.com";
 export default BASE_URL;
